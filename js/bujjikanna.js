a7806801f158d6d496c25514cfa1dbfad064bfa4
nirvan7 <75612989+nirvan7@users.noreply.github.com>
2021-02-02 18:59:10 +0530

    Add files via upload

---
+++ js/bujjikanna.js
@@ -0,0 +1,31 @@
+class Bujjikanna{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness:  0.004,
+            length: 5
+        }
+        this.pointB=pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+        if (this.sling.bodyA){
+        
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+ }
+    fly(){
+        this.sling.bodyA=null
+    }
+
+   attach(bodyA) {
+        this.sling.bodyA=stoneObj.body;
+
+    }
+}
